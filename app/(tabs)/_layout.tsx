3b2f6d47620f339f35b4d1bc8c47c59ab29e4cb8
John Paul Guiuan <john.guiuan@sorsu.edu.ph>
2026-05-12 07:06:46 -0700

    Initial Commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,26 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabLayout () {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
+    <Tabs>
+      <Tabs.Screen name="index" options={{
+          headerShown:false,
+          tabBarIcon: () => (
+              <FontAwesome name = "home" color= "black" size={30}/>
+          )
+
+      }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs.Screen name="settings" options={{
+      headerTitle: "Settings Page",
+      tabBarIcon: () => (
+        <FontAwesome name = "cogs" color="black" size={30} />
+      )
+    }} />
     </Tabs>
-  );
+  )
 }
+
